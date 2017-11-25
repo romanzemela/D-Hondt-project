0d545e2ee7664a08fbda8bab908e0c900c365cd0
Matthew Beermaker <matpiwowarski7@gmail.com>
2017-11-25 22:07:56 +0100

    blokada blednych danych

--- project1/project1/main.cpp
+++ project1/project1/main.cpp
@@ -8,65 +8,81 @@
 
 using namespace std;
 
+void Pomoc()
+{
+    cout << endl << " WYZNACZANIE ILOŚCI MANDATÓW UZYSKANYCH W WYBORACH PARLAMENTARNYCH ( METODA D'HONDT)" << endl << " Jeżeli chcemy wykorzystać dane z pliku:" << endl << endl << " Pierwsza wartoscią wymaganą jest ilość mandatów." << endl << " Następne wartości są głosami na poszczególne partie. " << endl << " Wszystkie wartosci powinny byc oddzielone spacją." << endl << " Wszystkie wartosci powinny byc liczbami naturalnymi." << endl << " Ilosc partii nie powinna przekraczać 100." << endl << " Jeżeli chcemy wykorzystać dane z wejścia standardowego:" << endl << endl << " Wartosci podawane powinny byc liczbami naturalnymi.";
+}
+
+void Poprawnosc_danych(bool & poprawne_dane)
+{
+    if(!cin)
+    {
+        cout << endl << " Wpisales bledne dane" << endl;
+        poprawne_dane = false;
+    }
+}
+
 void Wyzerowanie_mandatow(unsigned int mandaty[ ], unsigned int iloscpartii)
 {
     for (int i = 0; i < iloscpartii; i++)
         mandaty[i] = 0;
 }
 
-bool Odczytaj_argumenty(int ile , char ** argumenty , string & szInput , string & szOutput)
+void Odczytaj_argumenty(int ile , char ** argumenty , string & szInput , string & szOutput)
 {
     const string ETYKIETAINPUT ("-i");
     const string ETYKIETAOUTPUT ("-o");
-    const int FLAGAINPUT = 1;
-    const int FLAGAOUTPUT = FLAGAINPUT << 1;
-    const int POPRAWNYWYNIK = FLAGAINPUT | FLAGAOUTPUT;
-    int wynik = 0;
+    const string ETYKIETAHELP ("-h");
     
     for(int i = 1; i < ile - 1; i++)
     {
         string arg (argumenty[i]);
+        
         if (arg == ETYKIETAINPUT)
-        {
             szInput = argumenty[i + 1];
-            wynik |= FLAGAINPUT;
-            
-        }
+        
         if (arg == ETYKIETAOUTPUT)
-        {
             szOutput = argumenty [i + 1];
-            wynik |= FLAGAOUTPUT;
-            
-        }
+        if (arg == ETYKIETAHELP)
+            Pomoc();
     }
     
-    if( wynik == POPRAWNYWYNIK)
-        return true;
-    else
-        return false;
 }
 
-void Wartosci_z_konsoli(unsigned int &iloscpartii, unsigned int &iloscmandatow, unsigned int glosy[ ])
+
+
+bool Wartosci_z_konsoli(unsigned int &iloscpartii, unsigned int &iloscmandatow, unsigned int glosy[ ])
 {
-    cout << "Podaj liczbe partii (max. 100)" << endl;
+    bool poprawne_dane = true;
+    
+    cout << " Podaj liczbe partii (max. 100)" << endl;
     cin >> iloscpartii;
     
-    cout << "Podaj liczbe mandatow" << endl;
-    cin >> iloscmandatow;
+    Poprawnosc_danych(poprawne_dane);
+    
+    if(poprawne_dane)
+    {
+        cout << " Podaj liczbe mandatow" << endl;
+        cin >> iloscmandatow;
+        Poprawnosc_danych(poprawne_dane);
+    }
+    
     
     for (int i = 0; i < iloscpartii; i++)
     {
-        cout << "Podaj liczbe glosow na partie " << i+1 << endl;
-        cin >> glosy[i];
+        if(poprawne_dane)
+        {
+            cout << " Podaj liczbe glosow na partie " << i+1 << endl;
+            cin >> glosy[i];
+            Poprawnosc_danych(poprawne_dane);
+        }
     }
-    if(!cin)
-    {
-        cout << endl << "wpisales bledne dane" << endl;
-    }
+    return poprawne_dane;
 }
 
-void Pobieranie_danych(string wejscie, unsigned int &iloscpartii, int MAX, unsigned int glosy[ ], unsigned int &iloscmandatow)
+bool Pobieranie_danych(string wejscie, unsigned int &iloscpartii, int MAX, unsigned int glosy[ ], unsigned int &iloscmandatow)
 {
+    bool poprawne_dane = true;
     ifstream plikwejscia;
     plikwejscia.open("../dat/" + wejscie + ".txt", ios::in);
     if( plikwejscia.good())
@@ -81,15 +97,19 @@ void Pobieranie_danych(string wejscie, unsigned int &iloscpartii, int MAX, unsig
         }
         
         if( plikwejscia.fail())
+        {
+            poprawne_dane = false;
             cout << " Zle zapisane dane w pliku tekstowym " << endl;
-        
+        }
         plikwejscia.close();
     }
     else
     {
-        cout << " Nie podano lub nie znaleziono pliku wejsciowego" << endl;
-        Wartosci_z_konsoli(iloscpartii, iloscmandatow, glosy);
+        cout << endl << " Nie podano lub nie znaleziono pliku wejsciowego" << endl;
+        if(!Wartosci_z_konsoli(iloscpartii, iloscmandatow, glosy))
+            poprawne_dane = false;
     }
+    return poprawne_dane;
 }
 
 float Nowa_wartosc(unsigned int glosy, unsigned int mandaty)
@@ -130,40 +150,40 @@ int main(int ile, char ** argumenty)
     const int MAX = 100;  // maksymalna ilosc partii
     unsigned int mandaty[MAX];
     unsigned int glosy[MAX];
-    double baza_glosow[MAX];
     unsigned int iloscpartii = 0;
     unsigned int iloscmandatow;
-    unsigned int miejscemax = 0;
 
     string nazwa_pliku_wejscia;
     string nazwa_pliku_wyjsca;
     
     Odczytaj_argumenty(ile, argumenty, nazwa_pliku_wejscia, nazwa_pliku_wyjsca);
-    Pobieranie_danych(nazwa_pliku_wejscia, iloscpartii, MAX, glosy, iloscmandatow);
-    Wyzerowanie_mandatow( mandaty, iloscpartii);
-    
-    // podzial mandatów metodą d'Hondta
-    
-    for (int i = 0; i < iloscpartii; i++)
-        baza_glosow[i] = glosy[i];
-    
-    for (int i = 0 ; i < iloscmandatow; i++)
+    if(Pobieranie_danych(nazwa_pliku_wejscia, iloscpartii, MAX, glosy, iloscmandatow))
     {
-        // szukanie najwiekszej wartosci
-        double maks = baza_glosow[0];
-        for (int j = 0 ; j< iloscpartii; j++)
-            if (maks <= baza_glosow[j])
-            {
-                maks = baza_glosow[j];
-                miejscemax = j;
-            }
-        
-        // partia z najwieksza wartoscia dostaje mandat
-        mandaty[miejscemax]++;
-        baza_glosow[miejscemax] = Nowa_wartosc(glosy[miejscemax], mandaty[miejscemax]);
-    }
+        Wyzerowanie_mandatow( mandaty, iloscpartii);
     
-    Wynik(nazwa_pliku_wyjsca, iloscpartii, mandaty);
-
+        // podzial mandatów metodą d'Hondta
+        double baza_glosow[MAX];
+        int miejscemax = 0;
+        for (int i = 0; i < iloscpartii; i++)
+            baza_glosow[i] = glosy[i];
+    
+        for (int i = 0 ; i < iloscmandatow; i++)
+        {
+            // szukanie najwiekszej wartosci
+            double maks = baza_glosow[0];
+            for (int j = 0 ; j< iloscpartii; j++)
+                if (maks <= baza_glosow[j])
+                {
+                    maks = baza_glosow[j];
+                    miejscemax = j;
+                }
+        
+            // partia z najwieksza wartoscia dostaje mandat
+            mandaty[miejscemax]++;
+            baza_glosow[miejscemax] = Nowa_wartosc(glosy[miejscemax], mandaty[miejscemax]);
+        }
+    
+        Wynik(nazwa_pliku_wyjsca, iloscpartii, mandaty);
+        }
     return 0;
 }
